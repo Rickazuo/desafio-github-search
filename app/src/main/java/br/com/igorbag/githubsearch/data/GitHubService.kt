03f94ba5d4ba68f7632aebdf3ab73869b8d35dfa
Rickazuo <rick3209@gmail.com>
2023-10-15 19:31:26 -0300

    end project

--- app/src/main/java/br/com/igorbag/githubsearch/data/GitHubService.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/data/GitHubService.kt
@@ -4,7 +4,6 @@ import br.com.igorbag.githubsearch.domain.Repository
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-
 interface GitHubService {
 
     @GET("users/{user}/repos")
--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -1,100 +1,185 @@
 package br.com.igorbag.githubsearch.ui
 
-import android.content.Intent
-import android.net.Uri
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+//import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+//import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.data.GitHubService
 import br.com.igorbag.githubsearch.domain.Repository
+import br.com.igorbag.githubsearch.ui.adapter.RepositoryAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var nomeUsuario: EditText
-    lateinit var btnConfirmar: Button
-    lateinit var listaRepositories: RecyclerView
+    lateinit var userName: EditText
+    lateinit var searchButton: Button
+    lateinit var listOfRepositories: RecyclerView
     lateinit var githubApi: GitHubService
+    lateinit var loading : ProgressBar
+    lateinit var icWifiOff : ImageView
+    lateinit var txtWifiOff : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         setContentView(R.layout.activity_main)
         setupView()
         showUserName()
         setupRetrofit()
-        getAllReposByUserName()
+        setupListeners()
     }
 
     // Metodo responsavel por realizar o setup da view e recuperar os Ids do layout
     fun setupView() {
-        //@TODO 1 - Recuperar os Id's da tela para a Activity com o findViewById
+        userName = findViewById(R.id.et_user_name)
+        searchButton = findViewById(R.id.search_button)
+        listOfRepositories = findViewById(R.id.rv_list_of_repositories)
+        loading = findViewById(R.id.loading)
+        icWifiOff = findViewById(R.id.iv_wifi_off)
+        txtWifiOff = findViewById(R.id.tv_wifi_off)
+    }
+
+    //Método responsável por fazer a configuração base do Retrofit
+    fun setupRetrofit() {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        githubApi = retrofit.create(GitHubService::class.java)
     }
 
     //metodo responsavel por configurar os listeners click da tela
     private fun setupListeners() {
-        //@TODO 2 - colocar a acao de click do botao confirmar
-    }
+        searchButton.setOnClickListener {
 
+            val connection = isInternetAvailable()
 
-    // salvar o usuario preenchido no EditText utilizando uma SharedPreferences
-    private fun saveUserLocal() {
-        //@TODO 3 - Persistir o usuario preenchido na editText com a SharedPref no listener do botao salvar
-    }
+            if (!connection) {
+                icWifiOff.isVisible = true
+                txtWifiOff.isVisible = true
+            } else {
 
-    private fun showUserName() {
-        //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a sharedpref possuir algum valor, exibir no proprio editText o valor salvo
-    }
+                icWifiOff.isVisible = false
+                txtWifiOff.isVisible = false
 
-    //Metodo responsavel por fazer a configuracao base do Retrofit
-    fun setupRetrofit() {
-        /*
-           @TODO 5 -  realizar a Configuracao base do retrofit
-           Documentacao oficial do retrofit - https://square.github.io/retrofit/
-           URL_BASE da API do  GitHub= https://api.github.com/
-           lembre-se de utilizar o GsonConverterFactory mostrado no curso
-        */
-    }
-
-    //Metodo responsavel por buscar todos os repositorios do usuario fornecido
-    fun getAllReposByUserName() {
-        // TODO 6 - realizar a implementacao do callback do retrofit e chamar o metodo setupAdapter se retornar os dados com sucesso
-    }
-
-    // Metodo responsavel por realizar a configuracao do adapter
-    fun setupAdapter(list: List<Repository>) {
-        /*
-            @TODO 7 - Implementar a configuracao do Adapter , construir o adapter e instancia-lo
-            passando a listagem dos repositorios
-         */
-    }
-
-
-    // Metodo responsavel por compartilhar o link do repositorio selecionado
-    // @Todo 11 - Colocar esse metodo no click do share item do adapter
-    fun shareRepositoryLink(urlRepository: String) {
-        val sendIntent: Intent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, urlRepository)
-            type = "text/plain"
+                val searchName = userName.text.toString()
+                getAllReposByUserName(searchName)
+                saveUserLocal()
+                listOfRepositories.isVisible = false //Para que desapareça quando for fazer nossas pesquisas
+            }
         }
-
-        val shareIntent = Intent.createChooser(sendIntent, null)
-        startActivity(shareIntent)
     }
 
-    // Metodo responsavel por abrir o browser com o link informado do repositorio
+    fun isInternetAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    // @Todo 12 - Colocar esse metodo no click item do adapter
-    fun openBrowser(urlRepository: String) {
-        startActivity(
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse(urlRepository)
-            )
-        )
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
 
+        return actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+    }
+
+
+    // Método responsável por buscar todos os repositórios do usuário fornecido
+    fun getAllReposByUserName(userName: String) {
+
+        if (userName.isNotEmpty()) {
+
+            loading.isVisible = true
+
+            githubApi.getAllRepositoriesByUser(userName)
+                .enqueue(object : Callback<List<Repository>> {
+
+                    override fun onResponse(
+                        call: Call<List<Repository>>,
+                        response: Response<List<Repository>>
+                    ) {
+                        if (response.isSuccessful) {
+
+                            loading.isVisible = false
+                            listOfRepositories.isVisible = true
+
+                            val repositories = response.body()
+
+                            repositories?.let {
+                                setupAdapter(repositories)
+                            }
+
+                        } else {
+
+                            loading.isVisible = false
+
+                            val context = applicationContext
+                            Toast.makeText(context, R.string.response_error, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
+
+                        loading.isVisible = false
+
+                        val context = applicationContext
+                        Toast.makeText(context, R.string.response_error, Toast.LENGTH_LONG).show()
+                    }
+
+                })
+        }
+    }
+
+    // Método responsável por realizar a configuração do adapter
+    fun setupAdapter(list: List<Repository>) {
+
+        val adapter = RepositoryAdapter(
+            this, list)
+
+        listOfRepositories.adapter = adapter
+    }
+
+    // Método responsável por salvar o usuário preenchido no EditText utilizando uma SharedPreferences
+    private fun saveUserLocal() {
+
+        val searchResult = userName.text.toString()
+
+        val sharedPreference = getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPreference.edit()) {
+            putString("saved_username", searchResult)
+            apply()
+        }
+    }
+
+    //Método que exibe o que foi salvo na SharedPreference
+    private fun showUserName() {
+
+        val sharedPreference = getPreferences(MODE_PRIVATE) ?: return
+        val lastSearch = sharedPreference.getString("saved_username", null)
+
+        if (!lastSearch.isNullOrEmpty()) {
+            userName.setText(lastSearch)
+        }
     }
 
 }
